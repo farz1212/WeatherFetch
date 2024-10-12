d46d661ba9c8921d7a62d0710c099e332310260e
farz1212 <farzaad1212@gmail.com>
2024-10-12 11:39:11 -0400

    - Removed extra view - Less user interaction required - Location Persists through sessions using UserDefaults

---
+++ WeatherFetch/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WeatherFetch
+//
+//  Created by Farzaad Goiporia on 2024-10-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
